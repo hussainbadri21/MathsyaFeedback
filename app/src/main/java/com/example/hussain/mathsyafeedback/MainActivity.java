c76b267e1505f77964edd1f3094ba85804cf1433
Hussain <hussain_bri2@yahoo.com>
2016-09-16 18:26:25 +0530

    Final Push

--- app/src/main/java/com/example/hussain/mathsyafeedback/MainActivity.java
+++ app/src/main/java/com/example/hussain/mathsyafeedback/MainActivity.java
@@ -1,37 +1,17 @@
 package com.example.hussain.mathsyafeedback;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
-import android.os.AsyncTask;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RatingBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.example.hussain.mathsyafeedback.R.id.ratingBar;
 
 public class MainActivity extends Activity {
 
@@ -60,13 +40,17 @@ String res="";
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        res= String.valueOf(rb1.getRating())+" "+String.valueOf(rb2.getRating())+" "+String.valueOf(rb3.getRating())+" "+String.valueOf(rb4.getRating())+" "+String.valueOf(rb5.getRating());
+                        if(rb1.getRating()==0 || rb2.getRating()==0|| rb3.getRating()==0|| rb4.getRating()==0|| rb5.getRating()==0)
+                            Toast.makeText(getApplicationContext(),"Please fill all Fields!",Toast.LENGTH_SHORT).show();
+                        else {
+                            res = String.valueOf(rb1.getRating()) + " " + String.valueOf(rb2.getRating()) + " " + String.valueOf(rb3.getRating()) + " " + String.valueOf(rb4.getRating()) + " " + String.valueOf(rb5.getRating());
 
 
-
-                        Intent intent = new Intent(MainActivity.this, test.class);
-        intent.putExtra("arg", res);
-        startActivity(intent);
+                            Intent intent = new Intent(MainActivity.this, testi.class);
+                            intent.putExtra("arg", res);
+                            startActivity(intent);
+                            finish();
+                        }
                     }
                 });
 
--- app/src/main/java/com/example/hussain/mathsyafeedback/Sender.java
+++ app/src/main/java/com/example/hussain/mathsyafeedback/Sender.java
@@ -1,8 +1,6 @@
 package com.example.hussain.mathsyafeedback;
 
-/**
- * Created by Hussain on 15-Jun-16.
- */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,74 +21,74 @@ import android.util.Log;
 public class Sender {
 
 
-        static InputStream is = null;
-        static String response = null;
-        public final static int GET = 1;
-        public final static int POST = 2;
+    static InputStream is = null;
+    static String response = null;
+    public final static int GET = 1;
+    public final static int POST = 2;
 
 
 
-        public String makeServiceCall(String url, int method) {
-            return this.makeServiceCall(url, method, null);
-        }
-
-
-        public String makeServiceCall(String url, int method,
-                                      List<NameValuePair> params) {
-            try {
-
-                DefaultHttpClient httpClient = new DefaultHttpClient();
-                HttpEntity httpEntity = null;
-                HttpResponse httpResponse = null;
-
-
-                if (method == POST) {
-                    HttpPost httpPost = new HttpPost(url);
-
-                    if (params != null) {
-                        httpPost.setEntity(new UrlEncodedFormEntity(params));
-                    }
-
-                    httpResponse = httpClient.execute(httpPost);
-
-                } else if (method == GET) {
-
-                    if (params != null) {
-                        String paramString = URLEncodedUtils
-                                .format(params, "utf-8");
-                        url += "?" + paramString;
-                    }
-                    HttpGet httpGet = new HttpGet(url);
-
-                    httpResponse = httpClient.execute(httpGet);
-
-                }
-                httpEntity = httpResponse.getEntity();
-                is = httpEntity.getContent();
-
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(
-                        is, "UTF-8"), 8);
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-                while ((line = reader.readLine()) != null) {
-                    sb.append(line + "\n");
-                }
-                is.close();
-                response = sb.toString();
-            } catch (Exception e) {
-                Log.e("Buffer Error", "Error: " + e.toString());
-            }
-
-            return response;
-        }
+    public String makeServiceCall(String url, int method) {
+        return this.makeServiceCall(url, method, null);
     }
+
+
+    public String makeServiceCall(String url, int method,
+                                  List<NameValuePair> params) {
+        try {
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpEntity httpEntity = null;
+            HttpResponse httpResponse = null;
+
+
+            if (method == POST) {
+                HttpPost httpPost = new HttpPost(url);
+
+                if (params != null) {
+                    httpPost.setEntity(new UrlEncodedFormEntity(params));
+                }
+
+                httpResponse = httpClient.execute(httpPost);
+
+            } else if (method == GET) {
+
+                if (params != null) {
+                    String paramString = URLEncodedUtils
+                            .format(params, "utf-8");
+                    url += "?" + paramString;
+                }
+                HttpGet httpGet = new HttpGet(url);
+
+                httpResponse = httpClient.execute(httpGet);
+
+            }
+            httpEntity = httpResponse.getEntity();
+            is = httpEntity.getContent();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            response = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error: " + e.toString());
+        }
+
+        return response;
+    }
+}
 
